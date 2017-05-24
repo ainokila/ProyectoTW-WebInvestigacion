3cfeecde2aea28b38596299a1d408088dc71496a
ainokila <universidadcvr@gmail.com>
2017-05-24 18:03:30 +0200

    Aniadido php

---
+++ php/principal.php
@@ -0,0 +1,7 @@
+<?php
+		include '../plantillas/head-principal.html';
+		include "../plantillas/cabecera-menu.html";
+		include "../plantillas/principal.html";
+		include "../plantillas/footer.html";
+
+?>
---
+++ php/proyectos.php
@@ -0,0 +1,63 @@
+<?php
+		include '../plantillas/head-proyecto.html';
+		include "../plantillas/cabecera-menu.html";
+		
+
+?>
+
+<div class="principal-der">
+			<div class="proyecto">
+				<ul>
+					<li>Código</li>
+					<li>Título</li>
+					<li>Descripción</li>
+					<li>
+						<table border="1">
+							<tr>
+								<th>Publicaciones</th>
+							</tr>
+							<tr>
+								<td>-<a href="../php/log.php">Referencia a Publicación 1</a></td>
+							</tr>
+							<tr>
+								<td>-<a href="../php/log.php">Referencia a Publicación 2</a></td>
+							</tr>
+						</table>
+					</li>
+					<li>
+						<table border="1">
+							<tr>
+								<th>Publicaciones</th>
+							</tr>
+							<tr>
+								<td>-<a href="../php/log.php">Referencia a Publicación 1</a></td>
+							</tr>
+							<tr>
+								<td>-<a href="../php/log.php">Referencia a Publicación 2</a></td>
+							</tr>
+						</table>
+					</li>
+					<li>
+						<table border="1">
+							<tr>
+								<th>Publicaciones</th>
+							</tr>
+							<tr>
+								<td>-<a href="../php/log.php">Referencia a Publicación 1</a></td>
+							</tr>
+							<tr>
+								<td>-<a href="../php/log.php">Referencia a Publicación 2</a></td>
+							</tr>
+						</table>
+					</li>
+				</ul>
+				<p></p>
+			</div>
+		</div>
+	</div>
+	
+
+<?php
+	include "../plantillas/footer.html";
+
+?>
