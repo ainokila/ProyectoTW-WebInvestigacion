51f0ea3571ce4dc875536558362806f45d104a9a
Saytes <saytes@correo.ugr.es>
2017-06-19 19:57:22 +0200

    Añadiendo botones

--- php/proyectos.php
+++ php/proyectos.php
@@ -9,51 +9,60 @@
 
 ?>
 
-<div class="principal-der">
+
+	<div class="principal-der">
 			<div class="proyecto">
 				<ul>
-					<li>Código</li>
-					<li>Título</li>
-					<li>Descripción</li>
+
+
+<?php
+
+	$db = BD_conexion();
+	$tabla = BD_getProyectos($db);
+	BD_desconexion($db);
+	
+	foreach ((array) $tabla as $valor) {
+  
+    	$codigo = $valor["codigo"];
+    	$titulo = $valor["titulo"];
+    	$descripcion = $valor["descripcion"];
+
+    	$db = BD_conexion();
+		$publicaciones = BD_getPublicacionConProye($db,$codigo);
+		BD_desconexion($db);
+
+
+echo<<<HTML
+    				<li>Código: $codigo</li>
+					<li>Título: $titulo</li>
+					<li>Descripción: $descripcion</li>
 					<li>
 						<table border="1">
 							<tr>
 								<th>Publicaciones</th>
 							</tr>
-							<tr>
-								<td>-<a href="../php/log.php">Referencia a Publicación 1</a></td>
-							</tr>
-							<tr>
-								<td>-<a href="../php/log.php">Referencia a Publicación 2</a></td>
-							</tr>
+HTML;
+		foreach ((array) $publicaciones as $line) {
+			$titulo = $line["titulo"];
+echo<<<HTML
+			<tr>
+				<td>$titulo</td>
+			</tr>
+HTML;
+
+		}
+
+echo<<<HTML
 						</table>
 					</li>
-					<li>
-						<table border="1">
-							<tr>
-								<th>Publicaciones</th>
-							</tr>
-							<tr>
-								<td>-<a href="../php/log.php">Referencia a Publicación 1</a></td>
-							</tr>
-							<tr>
-								<td>-<a href="../php/log.php">Referencia a Publicación 2</a></td>
-							</tr>
-						</table>
-					</li>
-					<li>
-						<table border="1">
-							<tr>
-								<th>Publicaciones</th>
-							</tr>
-							<tr>
-								<td>-<a href="../php/log.php">Referencia a Publicación 1</a></td>
-							</tr>
-							<tr>
-								<td>-<a href="../php/log.php">Referencia a Publicación 2</a></td>
-							</tr>
-						</table>
-					</li>
+
+HTML;
+
+    }
+
+
+?>
+
 				</ul>
 				<p></p>
 			</div>
