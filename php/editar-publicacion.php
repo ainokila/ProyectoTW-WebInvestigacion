fe1b9388263117eb29a97f697307a3c7e1f4fc75
Saytes <saytes@correo.ugr.es>
2017-06-19 16:06:48 +0200

    Añadiendo consultas

--- php/editar-publicacion.php
+++ php/editar-publicacion.php
@@ -9,13 +9,76 @@
 		
 
 ?>
-
-
 		<div class="principal-der">
 			<div id="publicaciones">
 				<p>Contenido</p>
 				<p>Listado de publicaciones del grupo</p>
 
+				<div class="publicacion">
+					<p>Proyecto NºXXXX</p>
+					
+					<div class="informacion">
+  
+  						<form action="../php/aniadeproyecto.php" method="get">
+  							<table border="0">
+  				              <tr>
+  				                <td>Código</td>
+  				                <td><input type="text" name="codigo"></td>
+  				              </tr>
+  
+  				              <tr>
+  				                <td>Título</td>
+  				                <td><input type="text" name="titulo"></td>
+  				              </tr>
+  
+  				              <tr>
+  				                <td>Descripción</td>
+  				                <td><input type="large-text" name="descripción"></td>
+  				              </tr>
+  
+  				              <tr>
+  				                <td>Comienzo</td>
+  				                <td><input type="date" name="comienzo"></td>
+  				              </tr>
+  
+  				              <tr>
+  				                <td>Finalización</td>
+  				                <td><input type="date" name="finalizacion"></td>
+  				              </tr>		
+  
+  				              <tr>
+  				                <td>Entidades Colaboradoras</td>
+  				                <td><input type="text" name="entidades"></td>
+  				              </tr>
+  
+  				              <tr>
+  				                <td>Cuantía Concedida</td>
+  				                <td><input type="number" name="cuantia"></td>
+  				              </tr>
+  
+  
+  				              <tr>
+  				                <td>Investigador Principal</td>
+  				                <td><input type="text" name="investigador"></td>
+  				              </tr>
+  
+  				              <tr>
+  				                <td>Investigadores Colaboradores</td>
+  				                <td><input type="text" name="investigador-col"></td>
+  				              </tr>
+  
+  				              <tr>
+  				                <td>URL</td>
+  				                <td><input type="URL" name="url"></td>
+  				              </tr>
+
+
+	            			</table>
+            			</form>
+					</div>
+
+				</div>
+				<div class="clear"></div>	
 <?php
 
 	$db = BD_conexion();
@@ -82,31 +145,22 @@ echo <<< HTML
 					              		<input type="submit" name="actualizar" value="Actualizar">
 					              		<input type="submit" name="eliminar" value="Eliminar">
 					              	</div>
+					              	</div>
 	            				</form>
 
 	            			</table>
-
-
-
-
 					</div>
-
 				</div>
+
 				<div class="clear"></div>
 
 HTML;
-
 	}
-
-
 ?>
-
-
+				</div>
 			</div>
 		</div>
-	</div>
-	
+
 <?php
 	include "../plantillas/footer.html";
-
 ?>
