e2e88fd50eef5d11b89256a2e82ad983addea8c1
Saytes <saytes@correo.ugr.es>
2017-06-20 12:27:16 +0200

    asd

--- php/publicaciones.php
+++ php/publicaciones.php
@@ -2,28 +2,17 @@
 		session_start();
 		include './include/sesion.php';
 		include '../plantillas/head-publicaciones.html';
-
 		//include "../plantillas/cabecera-menu.html";
 		generaCabecera();
 		generaMenu();
 		
-
 ?>
 		<div class="principal-der">
 			<div class="publicaciones">
 				<p>Listado de Publicaciones</p>
 				<ul>
-					<form action="../php/buscar-publicacion.php" method="get">
+					<form action="../php/buscar-publicacion.php" method="post">
 					<p>Formulario de Búsqueda</p>
-						<li>Por tipo:				                
-		                	<select>
-		                		<option selected value="vacia"></option>
-		                		<option value="publicacion1">Publicación 1</option>
-		                		<option value="publicacion2">Publicación 2</option>
-		                		<option value="publicacion3">Publicación 3</option>
-		                		<option value="publicacion4">Publicación 4</option>
-		                	</select>
-					    </li>
 						<li>Por autor
 							<input type="text" name="autor">
 						<li>
@@ -37,24 +26,49 @@
 							<input type="submit" name="buscar">
 						</li>
 					</form>
+				</ul>		
+
+
+<?php
+	$db = BD_conexion();
+	$tabla = BD_getPublicaciones($db);
+	BD_desconexion($db);
+	
+	foreach ((array) $tabla as $valor) {
+  
+    	$doi = $valor["doi"];
+    	$titulo = $valor["titulo"];
+    	$autores = $valor["autores"];
+    	$fechapub = $valor["DATE_FORMAT(fechapub, '%d/%m/%Y')"];
+echo<<<HTML
+				<ul>
+    				<li>DOI: $doi</li>
+					<li>Título: $titulo</li>
+					<li>Autor/Autores: $autores</li>
+					<li>Fecha Publicación: $fechapub</li>
+					<li>
 				</ul>
-				<table border="1">
-					<tr>
-						<th>Publicaciones</th>
-					</tr>
-					<tr>
-						<td>-<a href="../php/log.php">Referencia a Publicación 1</a></td>
-					</tr>
-					<tr>
-						<td>-<a href="../php/log.php">Referencia a Publicación 2</a></td>
-					</tr>
-				</table>
+HTML;
+		/*foreach ((array) $publicaciones as $line) {
+			$titulo = $line["titulo"];
+echo<<<HTML
+			<tr>
+				<td>$titulo</td>
+			</tr>
+HTML;
+		}*/
+echo<<<HTML
+					</li>
+HTML;
+    }
+?>
+
+				</ul>
 			</div>
 		</div>
 	</div>
 	
-	<div class="clear"></div>	
+
 <?php
 	include "../plantillas/footer.html";
-
 ?>
