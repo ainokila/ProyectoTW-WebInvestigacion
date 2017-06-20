c5ae43a683f116ff223eb4071379f26eb52d9804
Saytes <saytes@correo.ugr.es>
2017-06-20 20:39:40 +0200

    Fin

--- php/include/credenciales.php
+++ php/include/credenciales.php
@@ -1,9 +1,9 @@
 <?php
 
 	define("IP","localhost",TRUE);
-	define("NAME_BD","proyectoinvestigacion",TRUE);
-	define("USER_BD","root",TRUE);
-	define("PASS_BD","root",TRUE);
+	define("NAME_BD","saytes1617",TRUE);
+	define("USER_BD","saytes1617",TRUE);
+	define("PASS_BD","UzIKdC",TRUE);
 
 
 	$GLOBALS['instalado'] = false;
