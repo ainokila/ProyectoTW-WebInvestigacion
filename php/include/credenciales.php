ade2dde5d00abf27cdc14ae6714e0a8ddc72fb12
ainokila <universidadcvr@gmail.com>
2017-06-21 12:23:44 +0200

    Ad

--- php/include/credenciales.php
+++ php/include/credenciales.php
@@ -1,9 +1,9 @@
 <?php
 
 	define("IP","localhost",TRUE);
-	define("NAME_BD","proyectoinvestigacion",TRUE);
-	define("USER_BD","root",TRUE);
-	define("PASS_BD","root",TRUE);
+	define("NAME_BD","universidadcvr1617",TRUE);
+	define("USER_BD","universidadcvr1617",TRUE);
+	define("PASS_BD","obE9ni",TRUE);
 
 
 	$GLOBALS['instalado'] = false;
